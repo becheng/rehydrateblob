4a18322b3a2256801be1186b4dff62d2a8adb0b7
Ben Cheng <ben.cheng@microsoft.com>
2021-10-10 17:18:20 -0400

    update hello func

--- api-external/listBlobs/index.js
+++ api-external/listBlobs/index.js
@@ -9,5 +9,6 @@ module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
+        // this is a comment
     };
 }
